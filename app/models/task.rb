18b46a12c172fd0c1d693fe7e71d6226100a8f2b
pedro <pedro.amador1090@gmail.com>
2016-04-13 21:34:42 -0500

    agregamos los scopes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ActiveRecord::Base
 	validates :title, presence: true
 	belongs_to :list
+	scope :incomplete, -> {where("completed = 'f'")}
+
+	#def self.incomplete
+	#	Task.where("completed = 'f'")
+	#end
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -12,4 +12,17 @@ class TaskTest < ActiveSupport::TestCase
   	assert_not_nil tasks(:one).list
   end
 
+  test 'show incomplete tasks'do
+
+  	incomplete_tasks = Task.incomplete
+  	assert_includes incomplete_tasks, tasks(:one)
+
+  end
+
+  test 'show complete tasks' do
+  	skip 'pending'
+  	complete_tasks = Task.complete
+
+  end
+
 end
