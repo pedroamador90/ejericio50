ff68fb931e9a620d911ddfe3fcf74cc74280b578
pedro <pedro.amador1090@gmail.com>
2016-04-14 20:32:08 -0500

    validaciones

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+	validates :name, :last_name, :username, :email, presence: true
+	validates :username, :email, uniqueness: true
 	has_many :lists, dependent: :destroy
 end
