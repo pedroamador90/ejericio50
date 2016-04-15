4d25fc55950e0b3083e83fe897876c803978e84d
pedro <pedro.amador1090@gmail.com>
2016-04-14 19:55:06 -0500

    create user model and add has many relation

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ActiveRecord::Base
 	validates :title, presence: true
 	has_many :tasks, dependent: :destroy
-
+	belongs_to :user
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
+
+
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+  	@user = users(:one)
+  end
+
+
+  test 'delete tasks when list is deleted' do
+  	@user.destroy
+  	assert_empty List.where(user_id: @user.id)	
+  end
+
 end
